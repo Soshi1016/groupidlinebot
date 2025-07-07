b5d41d91ddf05a2da66c55d96ee147be22494e8d
Soshi1016 <soshikobayashi1016@gmail.com>
2025-07-07 13:35:30 +0900

    Add files via upload

---
+++ groupid_linebot.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, abort
+import os
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+# 環境変数から取得する方式に変更（Renderの環境変数設定に対応）
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
+
+if not LINE_CHANNEL_ACCESS_TOKEN or not LINE_CHANNEL_SECRET:
+    raise Exception("LINE_CHANNEL_ACCESS_TOKENとLINE_CHANNEL_SECRETを環境変数に設定してください")
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers.get('X-Line-Signature', '')
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.source.type == 'group':
+        group_id = event.source.group_id
+        reply_text = f"このグループのIDは: {group_id} です"
+    else:
+        reply_text = "グループ以外からのメッセージです。"
+
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
